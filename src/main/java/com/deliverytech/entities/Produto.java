9c73aa80c16dd5b91b3d1f179f3b9cde8994bc9d
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-26 18:28:36 -0300

    Entidade produto

---
+++ src/main/java/com/deliverytech/entities/Produto.java
@@ -0,0 +1,27 @@
+package com.deliverytech.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String categoria;
+    private String descricao;
+    private BigDecimal preco;
+    private Boolean disponivel = true;
+}
