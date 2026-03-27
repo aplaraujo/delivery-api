fa42fe57b20cb5c5b7f5b14b9f042909315a2a2a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-27 16:53:30 -0300

    Implementação de serviço: pedido

---
+++ src/main/java/com/deliverytech/services/impl/PedidoServiceImpl.java
@@ -0,0 +1,55 @@
+package com.deliverytech.services.impl;
+
+import com.deliverytech.entities.Pedido;
+import com.deliverytech.entities.StatusPedido;
+import com.deliverytech.repositories.PedidoRepository;
+import com.deliverytech.services.PedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PedidoServiceImpl implements PedidoService {
+    private final PedidoRepository pedidoRepository;
+
+    @Override
+    public Pedido criar(Pedido pedido) {
+        pedido.setStatus(StatusPedido.CRIADO);
+        return pedidoRepository.save(pedido);
+    }
+
+    @Override
+    public Optional<Pedido> buscarPorId(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    @Override
+    public List<Pedido> buscarPorClientes(Long clienteId) {
+        return pedidoRepository.findByClienteId(clienteId);
+    }
+
+    @Override
+    public List<Pedido> buscarPorRestaurantes(Long restauranteId) {
+        return pedidoRepository.findByRestauranteId(restauranteId);
+    }
+
+    @Override
+    public Pedido atualizarStatus(Long id, StatusPedido status) {
+        return pedidoRepository.findById(id)
+                .map(pedido -> {
+                    pedido.setStatus(status);
+                    return pedidoRepository.save(pedido);
+                }).orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+    }
+
+    @Override
+    public void cancelar(Long id) {
+        pedidoRepository.findById(id).ifPresent(pedido -> {
+            pedido.setStatus(StatusPedido.CANCELADO);
+            pedidoRepository.save(pedido);
+        });
+    }
+}
