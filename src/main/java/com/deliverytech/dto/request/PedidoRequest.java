d0c6774b051047fbf3bee4593e6184819a64c5ca
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-27 17:12:32 -0300

    DTO de requisição: pedido

---
+++ src/main/java/com/deliverytech/dto/request/PedidoRequest.java
@@ -0,0 +1,26 @@
+package com.deliverytech.dto.request;
+
+import com.deliverytech.entities.Endereco;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PedidoRequest {
+    @NotNull
+    private Long clienteId;
+
+    @NotNull
+    private Long restauranteId;
+
+    @NotNull
+    private Endereco enderecoEntrega;
+
+    @NotNull
+    private List<ItemPedidoRequest> itens;
+}
