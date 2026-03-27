66a3ccb54a94e6db3c2ec00b44beb30d2a177ddb
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-27 17:29:08 -0300

    DTO de resposta: produto

---
+++ src/main/java/com/deliverytech/dto/response/ProdutoResponse.java
@@ -0,0 +1,19 @@
+package com.deliverytech.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoResponse {
+    private Long id;
+    private String nome;
+    private String categoria;
+    private String descricao;
+    private BigDecimal preco;
+    private Boolean disponivel;
+}
