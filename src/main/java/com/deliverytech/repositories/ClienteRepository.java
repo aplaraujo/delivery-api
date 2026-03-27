1b86dae431d52c840bf0130032b6bceb60b6de0e
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-27 15:56:04 -0300

    Repositório do cliente

---
+++ src/main/java/com/deliverytech/repositories/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.deliverytech.repositories;
+
+import com.deliverytech.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<Cliente> findByAtivoTrue();
+}
