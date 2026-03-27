a3806da6d9bcce7092528d1ca838d470d95a2393
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-27 16:49:15 -0300

    Implementação de serviço: cliente

---
+++ src/main/java/com/deliverytech/services/impl/ClienteServiceImpl.java
@@ -0,0 +1,56 @@
+package com.deliverytech.services.impl;
+
+import com.deliverytech.entities.Cliente;
+import com.deliverytech.repositories.ClienteRepository;
+import com.deliverytech.services.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteServiceImpl implements ClienteService {
+    private final ClienteRepository clienteRepository;
+
+    @Override
+    public Cliente cadastrar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    @Override
+    public List<Cliente> buscarPorAtivos() {
+        return clienteRepository.findByAtivoTrue();
+    }
+
+    @Override
+    public Cliente atualizar(Long id, Cliente clienteAtualizado) {
+        return clienteRepository.findById(id)
+                .map(cliente -> {
+                    cliente.setNome(clienteAtualizado.getNome());
+                    return clienteRepository.save(cliente);
+                }).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
+    @Override
+    public void ativarDesativar(Long id) {
+        clienteRepository.findById(id).ifPresent(cliente -> {
+            cliente.setAtivo(!cliente.getAtivo());
+            clienteRepository.save(cliente);
+        });
+    }
+
+    private void simulateDelay() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
