460f1333efc154e1b809d7106512dffa3569a64b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-27 15:58:21 -0300

    Repositório do restaurante

---
+++ src/main/java/com/deliverytech/repositories/RestauranteRepository.java
@@ -0,0 +1,11 @@
+package com.deliverytech.repositories;
+
+import com.deliverytech.entities.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+    List<Restaurante> findByCategoria(String categoria);
+    List<Restaurante> findByAtivoTrue();
+}
